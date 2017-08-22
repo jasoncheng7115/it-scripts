21e52a805f7e9ddb8d5287413fcfefe0cd6c05ce
Jason Cheng <sanyu3u@gmail.com>
2017-08-22 13:23:33 +0800

    增加註解

--- proxmoxve/pve02mig.sh
+++ proxmoxve/pve02mig.sh
@@ -8,8 +8,11 @@ then
     echo "Migrate [102] From pve01 to pve02(me)..."
     echo
     pvecm expected 1
+    
+    ## copy vm config to pve02, then you can start it.
     mv /etc/pve/nodes/pve01/qemu-server/102.conf \
     /etc/pve/nodes/pve02/qemu-server/
+    
     echo
 else
     echo
