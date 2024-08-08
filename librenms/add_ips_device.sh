c2f91635c0656c1871efa36c064848f2890ca8f2
Jason Cheng <30381035+jasoncheng7115@users.noreply.github.com>
2024-08-08 16:43:31 +0800

    Update add_ips_device.sh

--- librenms/add_ips_device.sh
+++ librenms/add_ips_device.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Author: Jason Cheng (jason@jason.tools)
-# Description: Batch add devices within a given IP range to LibreNMS using specified SNMP version and community.
+# Description: Batch add devices from multiple specified subnets to LibreNMS using specified SNMP version and community.
 # Requires to be run as the 'librenms' user.
 
 # Check if the script is run as the 'librenms' user
@@ -9,41 +9,46 @@ if [ "$(whoami)" != "librenms" ]; then
     exit 1
 fi
 
-# Check if sufficient parameters are provided
-if [ "$#" -ne 4 ]; then
-    echo "Usage: $0 <start_ip> <end_ip> <snmp_version> <community>"
-    echo "Example: $0 192.168.1.1 192.168.1.254 v2c public"
+# Check if minimum parameters are provided
+if [ "$#" -lt 3 ]; then
+    echo "Usage: $0 <snmp_version> <community> <subnet1> [<subnet2> ...]"
+    echo "Example: $0 v2c public 192.168.1.0/24 192.168.2.0/24"
     exit 1
 fi
 
-start_ip=$1
-end_ip=$2
-snmp_version=$3
-community=$4
+snmp_version=$1
+community=$2
+shift 2 # Remove the first two arguments, rest are subnets
 
-# Convert IP to integer
-ip_to_int() {
-    local IFS=.
-    read ip1 ip2 ip3 ip4 <<< "$1"
-    echo $((ip1 * 256 ** 3 + ip2 * 256 ** 2 + ip3 * 256 + ip4))
+# Function to convert CIDR to a range of IPs
+cidr_to_ips() {
+    local cidr=$1
+    IFS='/' read -r ip mask <<< "$cidr"
+    local IFS='.'; read -r i1 i2 i3 i4 <<< "$ip"
+    local ip_num=$((i1 * 256**3 + i2 * 256**2 + i3 * 256 + i4))
+    local range=$((2**(32-mask)))
+    echo $(seq $((ip_num+1)) $((ip_num+range-2)))
 }
 
-# Convert integer back to IP
-int_to_ip() {
-    local ui32=$1
-    local ip1=$((ui32 >> 24 & 255))
-    local ip2=$((ui32 >> 16 & 255))
-    local ip3=$((ui32 >> 8 & 255))
-    local ip4=$((ui32 & 255))
-    echo "$ip1.$ip2.$ip3.$ip4"
+# Main loop to add devices within a subnet
+add_subnet() {
+    local subnet=$1
+    local version=$2
+    local community=$3
+    for ip_num in $(cidr_to_ips $subnet); do
+        local ip1=$((ip_num >> 24 & 255))
+        local ip2=$((ip_num >> 16 & 255))
+        local ip3=$((ip_num >> 8 & 255))
+        local ip4=$((ip_num & 255))
+        local device_ip="$ip1.$ip2.$ip3.$ip4"
+        # Add device
+        echo "Adding device: $device_ip"
+        lnms device:add $device_ip --$version -c $community
+    done
 }
 
-# Main loop
-start=$(ip_to_int $start_ip)
-end=$(ip_to_int $end_ip)
-for ip in $(seq $start $end); do
-    device_ip=$(int_to_ip $ip)
-    # Add device
-    echo "Adding device: $device_ip"
-    lnms device:add $device_ip --$snmp_version -c $community
+# Process each subnet
+for subnet in "$@"; do
+    echo "Processing subnet: $subnet"
+    add_subnet $subnet $snmp_version $community
 done
